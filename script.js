a89023fa4df2c1bdfb3c9a83d46ab9284b0b21ba
Joshua Vance <skinsfandc0612@icloud.com>
2019-02-07 16:23:30 -0500

    added underScore to match input from user word

--- script.js
+++ script.js
@@ -1,72 +1,48 @@
 //create function to generate of words but would like to be able to type any word in the dictionary
 let word = "";
 let guess = "";
-let alphabet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-];
+var text1 = document.querySelector(".letters");
 
 //when "new word" button is clicked then a prompt for user input is initiated.
 
 let newWord = document.querySelector(".newWord");
+
 newWord.addEventListener("click", function() {
   word = prompt("Please enter a new word!");
+  console.log("word>", word);
+
+  alert("hey");
+  let underScore = "_";
+  console.log(underScore);
+
+  for (let i = 0; i < 7; i++) {
+    underScore += "_";
+  }
+
+  text1.innerText = underScore;
+  console.log(text1.innerHTML);
 });
 
 //create uderscores based on length of the word that is typed into the box
 
-let underscore = [];
-for (let i = 0; i < word.length; i++) {
-  underscore.push("_");
-}
-console.log(underscore);
-guess = setGuess(word);
-console.log(guess);
-console.log(guess.length);
-// });
+// guess = setGuess(word);
+console.log("guess", guess);
+// console.log(guess.length);
+//});
 
 // when a user inputs a word (with prompt), make userGuess an equal length with underscores
 //function setGuess(userInput)
 
-let underscores = "";
-for (let i = 0; i < userInput.length; i++) {
-  underscores += "_";
-}
-// return underscores;
+// let underScore = "";
+// for (let i = 0; i < word.length; i++) {
+//   underScore += "_";
+// }
+
+//return underscores;
 
 //Getting guess from the player and ensuring that it is a single character
 
 //user is able to click button on keyboard for the guess
-
-let pressLetter = document.getElementById("letters");
-letterA.addEventListener("click", function(event) {
-  //console.log(letterA.value);
-  document.getElementById("letterA").disabled = true;
-});
 
 //make buttons on keyboard only able to pressed once
 
