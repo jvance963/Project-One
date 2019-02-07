d62136e8b8cb9ee66952db923dec1d32109f8dc0
Joshua Vance <skinsfandc0612@icloud.com>
2019-02-07 07:56:14 -0500

    added in some JS directions and deleted some previous JS

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 //create function to generate of words but would like to be able to type any word in the dictionary
 
-const word = ['mushroom', 'yoshi', 'princess', 'bowser'];
+const word = ["mushroom", "yoshi", "princess", "bowser"];
 
-let randNum = Math.floor(Math.random() * word.length)
+let randNum = Math.floor(Math.random() * word.length);
 let newWord = word[randNum];
-let underscore = []
+let rightWord = [];
+let wrongWord = [];
+let underScore = [];
+
+let docUnderScore = document.getElementsByClassName("underscore");
+
+console.log(newWord);
 
 //create uderscores based on length of the word that is typed into the box
 
-let underscore = () => {
-    for(let i =0; < newWord.length; i++) {
-        underscore.push['_'];
-    }
-    return underscore;
+let assignunderScore = () => {
+  for (let i = 0; i < newWord.length; i++) {
+    underScore.push["_"];
+  }
+  return underScore;
+};
+
+console.log(assignunderScore());
+
+// Make bowser jump when user does not guess word correctly because Mario died
+
+// Make Luigi jump when user does guess word correctly because Mario was saved
+
+function jumpHigh() {
+  var luigi = luigi;
 }
 
-console.log(underscore());
+//if user guesses the correct word then an alert box appears and says "Your answer is correct"
 
-//create the keyboard for the letters that is able to be clicked and guess letter
-//make guess on letters only able to be pressed once
-//if right push to the wrong array
-//if right push to the right array
+//if user guesses the incorrect answer then an alert box appears and says "Your answee is incorrect"
+
+// if user does not guess correctly then head appears
+
+// if user does not guess correctly then left arm appears
+
+// if user does not guess correctly then right arm appears
+
+// if user does not guess correctly then torso appears
+
+// if user does not guess correctly then left leg appears
+
+// if user does not guess correctly then right leg appears
+
+// if user does not guess correctly then left foot appears
+
+// if user does not guess correctly then right foot appears
