69e738449fe35f732a8faf0bf4c4601b3e0b7b95
Joshua Vance <skinsfandc0612@icloud.com>
2019-02-08 12:23:26 -0500

    changed pictures to register in website and created for loop for click letters

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-//create function to generate of words but would like to be able to type any word in the dictionary
 let word = "";
 let guess = "";
 var text1 = document.querySelector(".underscore");
-
-//when "new word" button is clicked then a prompt for user input is initiated.
 
 let newWord = document.querySelector(".newWord");
 
@@ -11,7 +8,6 @@ newWord.addEventListener("click", function() {
   word = prompt("Please enter a new word!");
   console.log(word);
 
-  //create uderscores based on length of the word that is typed into the box
   let underScore = "_";
   console.log(underScore);
 
@@ -22,64 +18,10 @@ newWord.addEventListener("click", function() {
   console.log(text1.innerHTML);
 });
 
-// guess = setGuess(word);
-//console.log("guess", guess);
-// console.log(guess.length);
-//});
+var letters = document.querySelectorAll("#letters a");
 
-//user is able to click button on keyboard for the guess
-
-// //document.querySelector(".letters");
-// letterA.addEventListener("click", function(event) {
-//   //console.log(letters.value);
-//   document.querySelector(".letters").disabled = true;
-// });
-
-//Getting guess from the player and ensuring that it is a single character
-
-var guess;
-var guessLetter;
-
-// let guessArray = guess(wordArray.length);
-// for (let i = 1; i < wordArray.length; i++) {
-//   if (word === wordArray[i]) {
-//     guessArray[i] = word;
-//     console.log(word);
-//   }
-// }
-
-//make buttons on keyboard only able to pressed once
-
-var checkLetter = function(letters) {
-  console.log(letters);
-};
-
-const letters = document.getElementById("letters");
-
-letters.addEventListener("click", function(event) {
-  //console.log("test");
-});
-
-// Make bowser jump when user does not guess word correctly because Mario died
-
-// Make Luigi jump when user does guess word correctly because Mario was saved
-
-//if user guesses the correct word then an alert box appears and says "Your answer is correct"
-
-//if user guesses the incorrect answer then an alert box appears and says "Your answer is incorrect"
-
-// if user does not guess correctly then head appears
-
-// if user does not guess correctly then left arm appears
-
-// if user does not guess correctly then right arm appears
-
-// if user does not guess correctly then torso appears
-
-// if user does not guess correctly then left leg appears
-
-// if user does not guess correctly then right leg appears
-
-// if user does not guess correctly then left foot appears
-
-// if user does not guess correctly then right foot appears
+for (let i = 0; i < letters.length; i++) {
+  letters[i].addEventListener("click", function(e) {
+    console.log(e.target.innerText);
+  });
+}
